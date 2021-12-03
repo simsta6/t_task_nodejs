b313b7b40b4b51df71b39ee30d9f069f2a6a8123
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-12-04 00:40:46 +0200

    Refactor: simple refactor

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -12,6 +12,15 @@ export class Item {
     }
 }
 
+const fixItemQuality = (item: Item) => {
+    if (item.quality > 50) {
+        item.quality = 50;
+    } else if (item.quality < 0) {
+        item.quality = 0;
+    }
+    return item;
+};
+
 export class GildedRose {
     items: Array<Item>;
 
@@ -21,48 +30,33 @@ export class GildedRose {
 
     updateQuality() {
         this.items.forEach(item => {
-            if (item.name != BRIE && item.name != BACKSTAGE_PASS) {
-                if (item.quality > 0) {
-                    if (item.name != SULFURAS) {
-                        item.quality = item.quality - 1;
-                    }
+            switch(item.name) {
+            case BRIE:
+                item.quality += item.sellIn > 0 ? 1 : 2;
+                item = fixItemQuality(item);
+                break;
+            case BACKSTAGE_PASS:
+                if (item.sellIn > 10) {
+                    item.quality += 1;
+                } else if (item.sellIn > 5 && item.sellIn < 11) {
+                    item.quality += 2;
+                } else if (item.sellIn > 0 && item.sellIn < 6) {
+                    item.quality += 3;
+                } else {
+                    item.quality = 0;
                 }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                    if (item.name == BACKSTAGE_PASS) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
+                item = fixItemQuality(item);
+                break;
+            case SULFURAS:
+                break;
+            default:
+                item.quality -= item.sellIn > 0 ? 1 : 2;
+                item = fixItemQuality(item);
+                break;
             }
+
             if (item.name != SULFURAS) {
                 item.sellIn = item.sellIn - 1;
-            }
-            if (item.sellIn < 0) {
-                if (item.name != BRIE) {
-                    if (item.name != BACKSTAGE_PASS) {
-                        if (item.quality > 0) {
-                            if (item.name != SULFURAS) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
             }
         });
 
