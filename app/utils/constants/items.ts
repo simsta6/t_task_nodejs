f48632c958080dc5ec6d79a2d500d486ca039787
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-12-03 23:53:50 +0200

    Chore: changed Conjured full name

--- app/utils/constants/items.ts
+++ app/utils/constants/items.ts
@@ -1,4 +1,4 @@
 export const BRIE = 'Aged Brie';
 export const BACKSTAGE_PASS = 'Backstage passes to a TAFKAL80ETC concert';
 export const SULFURAS = 'Sulfuras, Hand of Ragnaros';
-export const CONJURED = 'Conjured';
+export const CONJURED = 'Conjured Mana Cake';
