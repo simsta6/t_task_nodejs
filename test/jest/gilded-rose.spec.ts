dc2d49f269f258acda64e7edb22824bdd5fc2add
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-12-04 12:58:15 +0200

    Test: refactored test to be uniform for every item

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -3,43 +3,35 @@ import { BACKSTAGE_PASS, BRIE, CONJURED, SULFURAS } from '@/utils/constants/item
 
 describe('Gilded Rose', () => {
 
-  it(`updateQuality should never change ${SULFURAS} item properties`, () => {
-    const initialItems: Item[] = [
-      new Item(SULFURAS, 1, 80),
-      new Item(SULFURAS, 0, 80),
-      new Item(SULFURAS, -1, 80),
-    ];
-    const gildedRose = new GildedRose(initialItems);
+  const backstageTestData = [
+    { name: BACKSTAGE_PASS, sellIn: 12, quality:  0, daysToUpdate:  2, sellInAfter: 10, qualityAfter:  2}, //  sellIn > 10
+    { name: BACKSTAGE_PASS, sellIn: 10, quality:  0, daysToUpdate:  5, sellInAfter:  5, qualityAfter: 10}, //  5 < sellIn < 10
+    { name: BACKSTAGE_PASS, sellIn:  5, quality:  0, daysToUpdate:  5, sellInAfter:  0, qualityAfter: 15}, //  0 < sellIn < 5
+    { name: BACKSTAGE_PASS, sellIn: 15, quality:  0, daysToUpdate: 15, sellInAfter:  0, qualityAfter: 30}, //  0 < sellIn < 15
+    { name: BACKSTAGE_PASS, sellIn: 15, quality:  0, daysToUpdate: 16, sellInAfter: -1, qualityAfter:  0}, // -1 < sellIn < 15
+    { name: BACKSTAGE_PASS, sellIn: 10, quality: 50, daysToUpdate:  2, sellInAfter:  8, qualityAfter: 50}, //  quality > 50
+    { name: BACKSTAGE_PASS, sellIn:  0, quality:  0, daysToUpdate:  2, sellInAfter: -2, qualityAfter:  0}, //  quality < 0
+  ];
 
-    const days = 5;
-    [...Array(days)].forEach(() => {
-      const items = gildedRose.updateQuality();
-      items.forEach((item, index) => {
-        expect(item.name).toBe(initialItems[index].name);
-        expect(item.sellIn).toBe(initialItems[index].sellIn);
-        expect(item.quality).toBe(initialItems[index].quality);
-      });
-    });
-  });
+  const sulfurasTestData = [
+    { name: SULFURAS, sellIn:  1, quality:  80, daysToUpdate:  5, sellInAfter:  1, qualityAfter: 80},
+    { name: SULFURAS, sellIn:  0, quality:  80, daysToUpdate:  5, sellInAfter:  0, qualityAfter: 80},
+    { name: SULFURAS, sellIn: -1, quality:  80, daysToUpdate:  5, sellInAfter: -1, qualityAfter: 80},
+  ];
 
   [
-    { name: BACKSTAGE_PASS, sellIn: 12, quality:  0, daysToUpdate:  2, sellInAfter: 10, qualityAfter:  2},
-    { name: BACKSTAGE_PASS, sellIn: 10, quality:  0, daysToUpdate:  5, sellInAfter:  5, qualityAfter: 10},
-    { name: BACKSTAGE_PASS, sellIn:  5, quality:  0, daysToUpdate:  5, sellInAfter:  0, qualityAfter: 15},
-    { name: BACKSTAGE_PASS, sellIn:  1, quality: 50, daysToUpdate:  2, sellInAfter: -1, qualityAfter:  0},
-    { name: BACKSTAGE_PASS, sellIn: 15, quality:  0, daysToUpdate: 15, sellInAfter:  0, qualityAfter: 30},
-    { name: BACKSTAGE_PASS, sellIn: 15, quality:  0, daysToUpdate: 16, sellInAfter: -1, qualityAfter:  0},
-    { name: BACKSTAGE_PASS, sellIn: 10, quality: 50, daysToUpdate:  2, sellInAfter:  8, qualityAfter: 50},
-    { name: BACKSTAGE_PASS, sellIn:  0, quality:  0, daysToUpdate:  2, sellInAfter: -2, qualityAfter:  0},
+    ...sulfurasTestData,
+    ...backstageTestData
   ].forEach(item =>
-    it(`updateQuality should update ${BACKSTAGE_PASS} item quality to ${item.qualityAfter}`, () => {
-      const gildedRose = new GildedRose([item]);
+    it(`updateQuality should update ${item.name} item quality to ${item.qualityAfter} and sellIn to ${item.sellIn}`, () => {
+      const { name, sellIn, quality, sellInAfter, qualityAfter } = item;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
 
       [...Array(item.daysToUpdate)].forEach(() => gildedRose.updateQuality());
 
       const items = gildedRose.items;
-      expect(items[0].name).toBe(item.name);
-      expect(items[0].sellIn).toBe(item.sellInAfter);
-      expect(items[0].quality).toBe(item.qualityAfter);
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellInAfter);
+      expect(items[0].quality).toBe(qualityAfter);
     }));
 });
