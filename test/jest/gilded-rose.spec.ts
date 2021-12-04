13fad0952e27373dc1c72156511a91109fadafd7
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-12-04 11:54:48 +0200

    Test: Added Sulfuras item test case

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,24 @@
 import { Item, GildedRose } from '@/gilded-rose';
+import { BACKSTAGE_PASS, BRIE, CONJURED, SULFURAS } from '@/utils/constants/items';
 
 describe('Gilded Rose', () => {
-    it('should foo', () => {
-        const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toBe('fixme');
+
+  it(`updateQuality should never change ${SULFURAS} item properties`, () => {
+    const initialItems: Item[] = [
+      new Item(SULFURAS, 1, 80),
+      new Item(SULFURAS, 0, 80),
+      new Item(SULFURAS, -1, 80),
+    ];
+    const gildedRose = new GildedRose(initialItems);
+
+    const days = 5;
+    [...Array(days)].forEach(() => {
+      const items = gildedRose.updateQuality();
+      items.forEach((item, index) => {
+        expect(item.name).toBe(initialItems[index].name);
+        expect(item.sellIn).toBe(initialItems[index].sellIn);
+        expect(item.quality).toBe(initialItems[index].quality);
+      });
     });
+  });
 });
