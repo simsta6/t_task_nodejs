3b61bf61a635eebcfffbde569d18735bcca22f62
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-12-04 11:59:30 +0200

    Style: Reduced indent size from 4 to 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 13,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 13,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        '@typescript-eslint'
+    'quotes': [
+      'error',
+      'single'
     ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
 };
--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,62 +1,62 @@
 import { BACKSTAGE_PASS, BRIE, CONJURED, SULFURAS } from './utils/constants/items';
 
 export class Item {
-    name: string;
-    sellIn: number;
-    quality: number;
+  name: string;
+  sellIn: number;
+  quality: number;
 
-    constructor(name, sellIn, quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
 }
 
 export class GildedRose {
-    items: Array<Item>;
+  items: Array<Item>;
 
-    constructor(items = [] as Array<Item>) {
-        this.items = items;
-    }
+  constructor(items = [] as Array<Item>) {
+    this.items = items;
+  }
 
-    updateQuality() {
-        this.items.forEach(item => {
-            // default value for item quality change over time
-            let qualityChange = item.sellIn > 0 ? -1 : -2;
+  updateQuality() {
+    this.items.forEach(item => {
+      // default value for item quality change over time
+      let qualityChange = item.sellIn > 0 ? -1 : -2;
 
-            // Special cases
-            switch(item.name) {
-            case SULFURAS:
-                return;
-            case BRIE:
-                qualityChange = item.sellIn > 0 ? 1 : 2;
-                break;
-            case BACKSTAGE_PASS:
-                if (item.sellIn > 10) {
-                    qualityChange = 1;
-                } else if (item.sellIn > 5 && item.sellIn < 11) {
-                    qualityChange = 2;
-                } else if (item.sellIn > 0 && item.sellIn < 6) {
-                    qualityChange = 3;
-                } else {
-                    qualityChange = -item.quality; //sets quality to zero
-                }
-                break;
-            case CONJURED:
-                qualityChange *= 2;
-                break;
-            }
+      // Special cases
+      switch (item.name) {
+      case SULFURAS:
+        return;
+      case BRIE:
+        qualityChange = item.sellIn > 0 ? 1 : 2;
+        break;
+      case BACKSTAGE_PASS:
+        if (item.sellIn > 10) {
+          qualityChange = 1;
+        } else if (item.sellIn > 5 && item.sellIn < 11) {
+          qualityChange = 2;
+        } else if (item.sellIn > 0 && item.sellIn < 6) {
+          qualityChange = 3;
+        } else {
+          qualityChange = -item.quality; //sets quality to zero
+        }
+        break;
+      case CONJURED:
+        qualityChange *= 2;
+        break;
+      }
 
-            item.quality += qualityChange;
-            if (item.quality > 50) {
-                item.quality = 50;
-            } else if (item.quality < 0) {
-                item.quality = 0;
-            }
+      item.quality += qualityChange;
+      if (item.quality > 50) {
+        item.quality = 50;
+      } else if (item.quality < 0) {
+        item.quality = 0;
+      }
 
-            item.sellIn -= 1;
-        });
+      item.sellIn -= 1;
+    });
 
-        return this.items;
-    }
+    return this.items;
+  }
 }
