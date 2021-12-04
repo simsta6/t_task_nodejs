006818283bcc442260bb006cc119ad3271353b78
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-12-04 23:40:39 +0200

    Fix: fetchAnswer no longer returns empty string on error

--- app/services/yesno-api.ts
+++ app/services/yesno-api.ts
@@ -8,13 +8,13 @@ interface YesNoResponse {
 
 const URL = 'https://yesno.wtf/api';
 
-export const fetchAnswer = async (): Promise<string> => {
+export const fetchAnswer = async (): Promise<string | undefined> => {
   try {
     const response = await fetch(URL);
     const data: YesNoResponse = await response.json();
     return data.answer;
   } catch (err) {
     console.log(err);
-    return '';
+    return;
   }
 };
