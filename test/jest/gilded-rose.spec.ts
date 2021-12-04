cbd40e0557a9d4d22ad57476997625a044706801
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-12-04 13:24:56 +0200

    Refactor: changed test data structure

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,45 +1,9 @@
 import { Item, GildedRose } from '@/gilded-rose';
-import { BACKSTAGE_PASS, BRIE, CONJURED, SULFURAS } from '@/utils/constants/items';
+import {
+  sulfurasTestData, brieTestData, conjuredTestData, backstageTestData, defaultTestData
+} from './data/gilded-rose-data';
 
 describe('updateQuality', () => {
-
-  const backstageTestData = [
-    { name: BACKSTAGE_PASS, sellIn: 12, quality:  0, daysToUpdate:  2, sellInAfter: 10, qualityAfter:  2 }, //  sellIn > 10
-    { name: BACKSTAGE_PASS, sellIn: 10, quality:  0, daysToUpdate:  5, sellInAfter:  5, qualityAfter: 10 }, //  5 < sellIn < 10
-    { name: BACKSTAGE_PASS, sellIn:  5, quality:  0, daysToUpdate:  5, sellInAfter:  0, qualityAfter: 15 }, //  0 < sellIn < 5
-    { name: BACKSTAGE_PASS, sellIn: 15, quality:  0, daysToUpdate: 15, sellInAfter:  0, qualityAfter: 30 }, //  0 < sellIn < 15
-    { name: BACKSTAGE_PASS, sellIn: 15, quality:  0, daysToUpdate: 16, sellInAfter: -1, qualityAfter:  0 }, // -1 < sellIn < 15
-    { name: BACKSTAGE_PASS, sellIn: 10, quality: 50, daysToUpdate:  2, sellInAfter:  8, qualityAfter: 50 }, //  quality > 50
-    { name: BACKSTAGE_PASS, sellIn:  0, quality:  0, daysToUpdate:  2, sellInAfter: -2, qualityAfter:  0 }, //  quality < 0
-  ];
-
-  const brieTestData = [
-    { name: BRIE, sellIn:  5, quality:  0, daysToUpdate:  5, sellInAfter:  0, qualityAfter:  5 }, //  sellIn is positive
-    { name: BRIE, sellIn:  0, quality:  0, daysToUpdate:  5, sellInAfter: -5, qualityAfter: 10 }, //  sellIn is negative
-    { name: BRIE, sellIn:  5, quality:  0, daysToUpdate: 10, sellInAfter: -5, qualityAfter: 15 }, //  sellIn is positive but becomes negative
-    { name: BRIE, sellIn:  1, quality: 50, daysToUpdate:  1, sellInAfter:  0, qualityAfter: 50 }, //  quality > 50
-  ];
-
-  const conjuredTestData = [
-    { name: CONJURED, sellIn:  5, quality: 20, daysToUpdate:  5, sellInAfter:  0, qualityAfter: 10 }, //  sellIn is positive
-    { name: CONJURED, sellIn:  0, quality:  5, daysToUpdate:  5, sellInAfter: -5, qualityAfter:  0 }, //  sellIn is negative
-    { name: CONJURED, sellIn:  5, quality:  5, daysToUpdate: 10, sellInAfter: -5, qualityAfter:  0 }, //  sellIn is positive but becomes negative
-    { name: CONJURED, sellIn:  1, quality:  0, daysToUpdate:  1, sellInAfter:  0, qualityAfter:  0 }, //  quality < 0
-  ];
-
-  const sulfurasTestData = [
-    { name: SULFURAS, sellIn:  1, quality:  80, daysToUpdate:  5, sellInAfter:  1, qualityAfter: 80 },
-    { name: SULFURAS, sellIn:  0, quality:  80, daysToUpdate:  5, sellInAfter:  0, qualityAfter: 80 },
-    { name: SULFURAS, sellIn: -1, quality:  80, daysToUpdate:  5, sellInAfter: -1, qualityAfter: 80 },
-  ];
-
-  const defaultTestData = [
-    { name: 'Default', sellIn:  5, quality: 20, daysToUpdate:  5, sellInAfter:  0, qualityAfter: 15 }, //  sellIn is positive
-    { name: 'Default', sellIn:  0, quality:  5, daysToUpdate:  5, sellInAfter: -5, qualityAfter:  0 }, //  sellIn is negative
-    { name: 'Default', sellIn:  5, quality:  5, daysToUpdate: 10, sellInAfter: -5, qualityAfter:  0 }, //  sellIn is positive but becomes negative
-    { name: 'Default', sellIn:  1, quality:  0, daysToUpdate:  1, sellInAfter:  0, qualityAfter:  0 }, //  quality < 0
-  ];
-
   [
     ...sulfurasTestData,
     ...brieTestData,
