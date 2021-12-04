e0acda24f26dc4ece96fb6be255bd9603e2a2c7a
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-12-04 15:14:47 +0200

    Refactor: Added arguments to two functions so it is easier to unit test them. -Affected functions getCmdLineArguments, isCmdLineArgumentsValid

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,8 @@
 import { GildedRose } from './gilded-rose';
 import { mockedItems } from './utils/data/items';
 
-const isCmdLineArgumentsValid = (): boolean => {
-  const args = process.argv.slice(2);
+const isCmdLineArgumentsValid = (cmdLineArgs: string[]): boolean => {
+  const args = cmdLineArgs.slice(2);
 
   if (args.length != 2) {
     return false;
@@ -14,12 +14,12 @@ const isCmdLineArgumentsValid = (): boolean => {
   return !(isNaN(updateTimes) || isNaN(apiCallTimes)) ;
 };
 
-const getCmdLineArguments = (): { updateTimes: number, apiCallTimes: number } => {
-  if (!isCmdLineArgumentsValid()) {
+const getCmdLineArguments = (cmdLineArgs: string[]): { updateTimes: number, apiCallTimes: number } => {
+  if (!isCmdLineArgumentsValid(cmdLineArgs)) {
     console.log('This process should be called with two arguments, both need to be number');
     process.exit(1);
   }
-  const args = process.argv.slice(2);
+  const args = cmdLineArgs.slice(2);
 
   const updateTimes = +args[0];
   const apiCallTimes = +args[1];
