d50bb1016068e663f06342662059abe23693f706
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-12-04 12:42:53 +0200

    Test: Added Backstage Pass item test cases

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -21,4 +21,25 @@ describe('Gilded Rose', () => {
       });
     });
   });
+
+  [
+    { name: BACKSTAGE_PASS, sellIn: 12, quality:  0, daysToUpdate:  2, sellInAfter: 10, qualityAfter:  2},
+    { name: BACKSTAGE_PASS, sellIn: 10, quality:  0, daysToUpdate:  5, sellInAfter:  5, qualityAfter: 10},
+    { name: BACKSTAGE_PASS, sellIn:  5, quality:  0, daysToUpdate:  5, sellInAfter:  0, qualityAfter: 15},
+    { name: BACKSTAGE_PASS, sellIn:  1, quality: 50, daysToUpdate:  2, sellInAfter: -1, qualityAfter:  0},
+    { name: BACKSTAGE_PASS, sellIn: 15, quality:  0, daysToUpdate: 15, sellInAfter:  0, qualityAfter: 30},
+    { name: BACKSTAGE_PASS, sellIn: 15, quality:  0, daysToUpdate: 16, sellInAfter: -1, qualityAfter:  0},
+    { name: BACKSTAGE_PASS, sellIn: 10, quality: 50, daysToUpdate:  2, sellInAfter:  8, qualityAfter: 50},
+    { name: BACKSTAGE_PASS, sellIn:  0, quality:  0, daysToUpdate:  2, sellInAfter: -2, qualityAfter:  0},
+  ].forEach(item =>
+    it(`updateQuality should update ${BACKSTAGE_PASS} item quality to ${item.qualityAfter}`, () => {
+      const gildedRose = new GildedRose([item]);
+
+      [...Array(item.daysToUpdate)].forEach(() => gildedRose.updateQuality());
+
+      const items = gildedRose.items;
+      expect(items[0].name).toBe(item.name);
+      expect(items[0].sellIn).toBe(item.sellInAfter);
+      expect(items[0].quality).toBe(item.qualityAfter);
+    }));
 });
